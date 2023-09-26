f9c6abfd58e7efdc8ddd8cd0bd9767d746c0dc7d
honzbusinessman <114379144+honzbusinessman@users.noreply.github.com>
2023-09-26 14:43:03 +1300

    Add files via upload

---
+++ path2.ps1
@@ -0,0 +1,8 @@
+﻿$url      = 'https://www.callums-corner.com/wp-content/uploads/2017/08/self-defence-video.jpg'
+$req      = Invoke-WebRequest -Uri $url -OutFile 'C:\image.jpg'
+
+$regPath  = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+$keyName  = 'Wallpaper'
+$keyValue = 'C:\image.jpg'
+
+Set-ItemProperty -Path $regPath -Name $keyName -Value $keyValue
